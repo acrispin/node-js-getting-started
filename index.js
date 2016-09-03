5b9079eb65a3c65bfb5453c8438508368b58d881
Antonio Crispin <antonio.crispin@gmail.com>
2016-09-03 12:24:48 -0500

    update to basic

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/client'));
+
+/*
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -12,6 +15,7 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+*/
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
